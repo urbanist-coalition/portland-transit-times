90a08c3eb4b61f611c96d8bbe2454bbe71ab606c
Todd Morse <todd@morsecodist.io>
2025-12-06 14:15:20 -0500

    static map

--- src/app/by-location/page.tsx
+++ src/app/by-location/page.tsx
@@ -1,10 +1,5 @@
-import { getLines, getStops } from "@/lib/actions";
 import ByLocation from "@/components/by-location";
 
-export const dynamic = "force-dynamic";
-
-export default async function ByLocationPage() {
-  const allLines = await getLines();
-  const allStops = await getStops();
-  return <ByLocation allLines={allLines} allStops={allStops} />;
+export default function ByLocationPage() {
+  return <ByLocation />;
 }
