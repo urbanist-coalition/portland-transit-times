06dd5db6b16779ae9d046763152da774f0e88ee4
Todd Morse <todd@morsecodist.io>
2025-12-06 23:34:36 -0500

    Revert "static map"
    This reverts commit 90a08c3eb4b61f611c96d8bbe2454bbe71ab606c.

--- src/app/by-location/page.tsx
+++ src/app/by-location/page.tsx
@@ -1,5 +1,10 @@
+import { getLines, getStops } from "@/lib/actions";
 import ByLocation from "@/components/by-location";
 
-export default function ByLocationPage() {
-  return <ByLocation />;
+export const dynamic = "force-dynamic";
+
+export default async function ByLocationPage() {
+  const allLines = await getLines();
+  const allStops = await getStops();
+  return <ByLocation allLines={allLines} allStops={allStops} />;
 }
--- src/components/by-location.tsx
+++ src/components/by-location.tsx
@@ -3,20 +3,15 @@
 import { Box } from "@mui/material";
 import dynamic from "next/dynamic";
 import { Stop, RouteWithShape } from "@/types";
-import { getLines, getStops } from "@/lib/actions";
-import { useEffect, useState } from "react";
 
 const DynamicMap = dynamic(() => import("@/components/map"), { ssr: false });
 
-export default function ByLocation() {
-  const [allLines, setAllLines] = useState<RouteWithShape[]>([]);
-  const [allStops, setAllStops] = useState<Record<string, Stop>>({});
+interface ByLocationProps {
+  allLines: RouteWithShape[];
+  allStops: Record<string, Stop>;
+}
 
-  useEffect(() => {
-    getLines().then(setAllLines);
-    getStops().then(setAllStops);
-  }, []);
-
+export default function ByLocation({ allLines, allStops }: ByLocationProps) {
   return (
     <Box style={{ height: "100dvh", width: "100vw" }}>
       <DynamicMap allLines={allLines} allStops={allStops} />
